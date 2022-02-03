b693979cb84acdf0fd6e165ad96ab5261199a8eb
Alexandrmrk <markovan@lad24.ru>
2022-02-03 16:46:50 +0300

    fix

--- client/src/auth/api-auth.js
+++ client/src/auth/api-auth.js
@@ -1,6 +1,6 @@
 const signin = async (user) => {
   try {
-    let response = await fetch("http://176.119.147.35/auth/signin/", {
+    let response = await fetch("/auth/signin/", {
       method: "POST",
       headers: {
         Accept: "application/json",
