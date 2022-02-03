dfc48257da6b2aa1f2668507819680c3cc359846
Alexandrmrk <markovan@lad24.ru>
2022-02-03 20:46:49 +0300

    test

--- client/src/auth/api-auth.js
+++ client/src/auth/api-auth.js
@@ -1,15 +1,18 @@
 const signin = async (user) => {
   try {
-    let response = await fetch("https://polar-river-15204.herokuapp.com/api/signin", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      // credentials: "include",
-      body: JSON.stringify(user),
-    });
-
+    let response = await fetch(
+      "https://polar-river-15204.herokuapp.com/api/signin",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        // credentials: "include",
+        body: JSON.stringify(user),
+      }
+    );
+    console.log(response);
     return await response.json();
   } catch (err) {
     console.log(err);
